f1267c2e93708ba178e6beeb062f6c4bf42ccb08
williamistGitHub <william@distal.com>
2024-10-05 16:46:36 -0400

    add some docs

--- include/clog/clog.h
+++ include/clog/clog.h
@@ -26,6 +26,10 @@
 
 #include <stdarg.h>
 
+/**
+ * @brief Log levels supported by CLog.
+ * 
+ */
 typedef enum {
     CLOG_LEVEL_DEBUG = 0,
     CLOG_LEVEL_INFO = 1,
@@ -33,14 +37,38 @@ typedef enum {
     CLOG_LEVEL_ERROR = 3
 } clog_log_level_e;
 
+/**
+ * @brief Sets the current log level. Any messages "lower" than this will not be logged.
+ * 
+ * @param level The new log level.
+ */
 void clog_set_log_level(clog_log_level_e level);
+/**
+ * @brief Sets the current log file. Messages will be logged here as well if it is not null.
+ * 
+ * @param filename New log filename, `NULL` is accepted and means no log file.
+ */
 void clog_set_log_file(const char* filename);
 
 #ifdef __GNUC__
 __attribute__((format(printf, 2, 3)));
 #endif
+/**
+ * @brief Log, using varargs.
+ * 
+ * @param level Log level to use.
+ * @param fmt `printf`-style format string to log. `\\n` is appended automatically.
+ * @param ... Arguments to the format string.
+ */
 void clog_log(clog_log_level_e level, const char* fmt, ...);
 
+/**
+ * @brief Log, using `va_list`.
+ * 
+ * @param level Log level to use.
+ * @param fmt `printf`-style format string to log. \\n is appended automatically.
+ * @param args Arguments to the format string.
+ */
 void clog_logv(clog_log_level_e level, const char* fmt, va_list args);
 
 #endif
